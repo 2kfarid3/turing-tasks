ed24ff8a6d691f7cda348a41ed8516ffb6de2dfa
Farid <fkarimli047@gmail.com>
2024-01-24 19:09:58 +0400

    Files attached

---
+++ src/main/java/az/edu/turing/DiviserApp.java
@@ -0,0 +1,15 @@
+package az.edu.turing;
+
+import java.util.Scanner;
+
+public class DiviserApp {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
