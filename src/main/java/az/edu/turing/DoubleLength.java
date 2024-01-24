c03c511763be19b40dee5136ac0dbf9d396b3a12
Farid <fkarimli047@gmail.com>
2024-01-24 18:32:39 +0400

    Files attached

---
+++ src/main/java/az/edu/turing/DoubleLength.java
@@ -0,0 +1,11 @@
+package az.edu.turing;
+
+import java.util.Scanner;
+
+public class DoubleLength {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String word = in.nextLine();
+        System.out.println(word.length() * 2);
+    }
+}
