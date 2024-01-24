9ffd227a066cae3e909dabdcc9f39f6d270143f0
Farid <fkarimli047@gmail.com>
2024-01-24 20:36:08 +0400

    Files attached

---
+++ src/main/java/az/edu/turing/DoubleArrayElements.java
@@ -0,0 +1,17 @@
+package az.edu.turing;
+
+import java.util.Scanner;
+
+public class DoubleArrayElements {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int length = in.nextInt();
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++) {
+            array[i] = in.nextInt();
+        }
+        for (int i = 0; i < length; i++) {
+            System.out.print(array[i] * 2 + " ");
+        }
+    }
+}
