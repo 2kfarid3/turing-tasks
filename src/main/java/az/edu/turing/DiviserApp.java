40887a1811fee36d1e53e2d23cc34b6112101c9e
Farid <fkarimli047@gmail.com>
2024-01-28 20:16:45 +0400

    Files attached

--- src/main/java/az/edu/turing/DiviserApp.java
+++ src/main/java/az/edu/turing/DiviserApp.java
@@ -4,12 +4,22 @@ import java.util.Scanner;
 
 public class DiviserApp {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int num = in.nextInt();
-        for (int i = 1; i <= num; i++) {
-            if (num % i == 0) {
-                System.out.print(i + " ");
+                Scanner in = new Scanner(System.in);
+
+                // Input
+                System.out.println("Enter an integer:");
+                int number = in.nextInt();
+
+                // Find and print divisors using do-while loop
+                System.out.println("Divisors of " + number + ":");
+
+                int i = 1;
+                do {
+                    if (number % i == 0) {
+                        System.out.print(i + " ");
+                    }
+                    i++;
+                } while (i <= number/2;
             }
         }
-    }
-}
+
