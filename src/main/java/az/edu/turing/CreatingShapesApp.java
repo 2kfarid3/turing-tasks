3400e510ce4d19d89513edae49dfed15fd5fee06
Farid <fkarimli047@gmail.com>
2024-02-02 19:14:03 +0400

    Files attached

---
+++ src/main/java/az/edu/turing/CreatingShapesApp.java
@@ -0,0 +1,99 @@
+package az.edu.turing;
+
+import java.util.Scanner;
+
+public class CreatingShapesApp {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Count of the Rows:" + " ");
+        int k = in.nextInt();
+        int col = 1;
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print("x");
+            }
+            col++;
+            System.out.println();
+        }
+    }
+}
+
+/*public class CreatingShapesApp {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Count of the Rows:" + " ");
+        int k = in.nextInt();
+        int col = 1;
+        int space;
+        space = k-1;
+        for (int i = 0; i < k; i++) {
+            for (int z = 0; z < space; z++){
+                System.out.print(" ");
+            }
+            for (int j = 0; j < col; j++) {
+                System.out.print("x");
+            }
+            col++;
+            space--;
+            System.out.println();
+        }
+    }
+}
+
+
+public class CreatingShapesApp {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Count of the Rows:" + " ");
+        int k = in.nextInt();
+        int col = 1;
+        int space;
+        space = k-1;
+        for (int i = 0; i < k; i++) {
+            for (int z = 0; z < space; z++){
+                System.out.print(" ");
+            }
+            for (int j = 0; j < col; j++) {
+                System.out.print("x");
+            }
+            col+=2;
+            space--;
+            System.out.println();
+        }
+    }
+}
+
+public class CreatingShapesApp {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Count of the Rows:" + " ");
+        int k = in.nextInt();
+        int col = 1;
+        int space;
+        space = k-1;
+        for (int i = 0; i < k; i++) {
+            for (int z = 0; z < space; z++){
+                System.out.print(" ");
+            }
+            for (int j = 0; j < col; j++) {
+                System.out.print("x");
+            }
+            col+=2;
+            space--;
+            System.out.println();
+        }
+        col-=4;
+        space=1;
+        for (int i = 0; i < k; i++) {
+            for (int z = 0; z < space; z++){
+                System.out.print(" ");
+            }
+            for (int j = 0; j < col; j++) {
+                System.out.print("x");
+            }
+            col-=2;
+            space++;
+            System.out.println();
+        }
+    }
+}*/
---
+++ src/main/java/az/edu/turing/QuestionApp.java
@@ -0,0 +1,20 @@
+package az.edu.turing;
+
+import java.util.Scanner;
+
+public class AreaAndLength {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int P = 4 * a;
+        int A = a * a;
+        System.out.println("P:" + P);
+        System.out.println("A:" + A);
+        System.out.print("Write radius:" + " ");
+        double r = in.nextInt();
+        double L = 2 * Math.PI * r;
+        double Ar = Math.PI * r * r;
+        System.out.println("L:" + L);
+        System.out.println("Area of circle:" + Ar);
+    }
+}
