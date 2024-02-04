7c0e254731c30b646c4d9a6ed1dc329840e5abff
Farid <fkarimli047@gmail.com>
2024-02-05 01:42:28 +0400

    {'A', 'B', 'C'} --> ABC

---
+++ src/main/java/az/edu/turing/CharFunctionApp.java
@@ -0,0 +1,17 @@
+package az.edu.turing;
+
+
+public class CharFunctionApp {
+    public static void main(String[] args) {
+        char[] array = {'A', 'B', 'C'};
+        System.out.println(writeChar(array));
+    }
+
+    public static char[] writeChar(char[] arr) {
+        char[] myDigits = new char[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            myDigits[i] = arr[i];
+        }
+        return myDigits;
+    }
+}
