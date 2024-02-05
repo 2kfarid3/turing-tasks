ed83239c7190059d03fb37d742f0ff77fff94283
Farid <fkarimli047@gmail.com>
2024-02-05 16:28:04 +0400

    StringClassMethods(cutting name and finding length)---withoutvoid

--- src/main/java/az/edu/turing/StringClassMethods.java
+++ src/main/java/az/edu/turing/StringClassMethods.java
@@ -4,25 +4,27 @@ import java.util.Scanner;
 
 public class StringClassMethods {
     public static void main(String[] args) {
-        getStringWithoutEmail();
-        printStringLength();
+        Scanner in = new Scanner(System.in);
+        String b = in.nextLine();
+        String result = getName(b);
+        int result1 = getName1(b);
+        System.out.println(result);
+        System.out.println(result1);
     }
 
-    public static void getStringWithoutEmail(){
-        Scanner in = new Scanner(System.in);
-        String a = in.nextLine();
+    public static String getName(String a){
         int atIndex = a.indexOf("@");
         if (atIndex != -1) {
             String result = a.substring(0, atIndex);
-            System.out.println(result);
+            return result;
         } else {
             System.out.println("Invalid input!");
+            return null;
         }
     }
 
-    public static void printStringLength(){
-        Scanner in = new Scanner(System.in);
-        String a = in.nextLine();
-        System.out.println(a.length());
+    public static int getName1(String a){
+        return a.length();
     }
 }
+
