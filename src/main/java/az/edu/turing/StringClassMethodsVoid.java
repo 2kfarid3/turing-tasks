3007ae63542f0fb4e02edfff282192ea15c8b9aa
Farid <fkarimli047@gmail.com>
2024-02-05 16:32:10 +0400

    StringClassMethods(cutting name and finding length)---withvoid

---
+++ src/main/java/az/edu/turing/StringClassMethodsVoid.java
@@ -0,0 +1,29 @@
+package az.edu.turing;
+
+import java.util.Scanner;
+
+public class StringClassMethodsVoid {
+    public static void main(String[] args) {
+        getStringWithoutEmail();
+        printStringLength();
+    }
+
+    public static void getStringWithoutEmail(){
+        Scanner in = new Scanner(System.in);
+        String a = in.nextLine();
+        int atIndex = a.indexOf("@");
+        if (atIndex != -1) {
+            String result = a.substring(0, atIndex);
+            System.out.println(result);
+        } else {
+            System.out.println("Invalid input!");
+        }
+    }
+
+    public static void printStringLength(){
+        Scanner in = new Scanner(System.in);
+        String a = in.nextLine();
+        System.out.println(a.length());
+    }
+}
+
