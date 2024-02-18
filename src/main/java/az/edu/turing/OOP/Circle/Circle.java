ae01c0521975d5801be0aa2d65f780a69624d6e1
Farid <fkarimli047@gmail.com>
2024-02-19 03:45:03 +0400

    Creating a circle via OOP

---
+++ src/main/java/az/edu/turing/OOP/Circle/Circle.java
@@ -0,0 +1,34 @@
+package az.edu.turing.OOP.Circle;
+
+public class Circle {
+    public static long countOfCircles;
+    private  double radius;
+    private String color;
+
+    public Circle(double radius){
+        this.radius = radius;
+        countOfCircles++;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        if ("white".equals(color)){
+            this.color = color;
+        }
+    }
+
+    public double calcLength(){
+        return 2 * Math.PI * radius;
+    }
+}
---
+++ src/main/java/az/edu/turing/OOP/Circle/CircleApp.java
@@ -0,0 +1,8 @@
+package az.edu.turing.OOP.Circle;
+
+public class CircleApp {
+    public static void main(String[] args) {
+        final Circle c1  = new Circle(1);
+        System.out.println(c1.calcLength());
+    }
+}
